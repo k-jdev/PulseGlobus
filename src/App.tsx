d10c06393b5a328c35eabbbbf7de0db69ca5e791
k-jdev <svv.ooj@gmail.com>
2025-12-07 03:15:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { MapboxExample } from "./components";
+
+function App() {
+  return (
+    <>
+      <MapboxExample />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as MapboxExample } from "./MapboxExample";
