588b79dc2196ab452f7180491a216241b430e487
k-jdev <svv.ooj@gmail.com>
2025-12-28 20:32:29 +0200

    update: bg

--- src/components/MapboxExample/components/MapContainer.tsx
+++ src/components/MapboxExample/components/MapContainer.tsx
@@ -11,13 +11,15 @@ const containerStyle: CSSProperties = {
   position: "relative",
   width: "100%",
   height: "100vh",
-  backgroundColor: "#000000",
+  backgroundColor: "#1452F0",
 };
 
 const mapStyle: CSSProperties = {
   width: "100%",
   height: "100%",
   borderRadius: "0px",
+  position: "relative",
+  zIndex: 0,
 };
 
 const buttonStyle: CSSProperties = {
@@ -34,6 +36,19 @@ const buttonStyle: CSSProperties = {
   fontWeight: "500",
 };
 
+const dotsOverlayStyle: CSSProperties = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  pointerEvents: "none",
+  backgroundImage:
+    "radial-gradient(circle, rgba(255, 255, 255, 0.08) 2px, transparent 2px)",
+  backgroundSize: "20px 20px",
+  zIndex: 1,
+};
+
 export const MapContainer = ({
   mapContainerRef,
   theme,
@@ -46,6 +61,7 @@ export const MapContainer = ({
 
   return (
     <div style={containerStyle}>
+      {theme === "light" && <div style={dotsOverlayStyle} />}
       <div ref={mapContainerRef} style={mapStyle} />
       <button
         onClick={handleThemeToggle}
