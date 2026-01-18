379ffed8ba1521f71e7504cecc0338a388badf6f
k-jdev <svv.ooj@gmail.com>
2026-01-18 02:48:15 +0200

    fix: modal

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -43,5 +43,10 @@ createWeb3Modal({
   themeVariables: {
     "--w3m-accent": "#1452F0",
     "--w3m-z-index": 1000,
+    "--w3m-border-radius-master": "16px",
   },
+  featuredWalletIds: [
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // MetaMask
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // Trust Wallet
+  ],
 });
